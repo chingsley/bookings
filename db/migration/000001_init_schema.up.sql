87cc0606dc5ef9ac9b062b6aaab3883b3d414fc9
chingsley <eneja.kc@gmail.com>
2021-08-30 18:16:24 +0100

    - Setup the db driver

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE "users" (
   "id" bigserial PRIMARY KEY,
   "first_name" varchar NOT NULL,
   "last_name" varchar NOT NULL,
-  "email" varchar NOT NULL,
+  "email" VARCHAR (300) UNIQUE NOT NULL,
   "password" varchar NOT NULL,
   "access_level" bigint NOT NULL DEFAULT (1),
   "created_at" timestamptz NOT NULL DEFAULT (now()),
@@ -27,7 +27,7 @@ CREATE TABLE "reservations" (
   "id" bigserial PRIMARY KEY,
   "first_name" varchar NOT NULL,
   "last_name" varchar NOT NULL,
-  "email" varchar NOT NULL,
+  "email" VARCHAR (300) UNIQUE NOT NULL,
   "phone" varchar NOT NULL,
   "start_date" timestamptz NOT NULL,
   "end_date" timestamptz NOT NULL,
@@ -51,3 +51,11 @@ ALTER TABLE "reservations" ADD FOREIGN KEY ("room_id") REFERENCES "rooms" ("id")
 ALTER TABLE "room_restrictions" ADD FOREIGN KEY ("room_id") REFERENCES "rooms" ("id");
 ALTER TABLE "room_restrictions" ADD FOREIGN KEY ("reservation_id") REFERENCES "reservations" ("id");
 ALTER TABLE "room_restrictions" ADD FOREIGN KEY ("restriction_id") REFERENCES "restrictions" ("id");
+
+
+CREATE INDEX ON "room_restrictions" ("start_date");
+CREATE INDEX ON "room_restrictions" ("end_date");
+CREATE INDEX ON "room_restrictions" ("room_id");
+CREATE INDEX ON "room_restrictions" ("reservation_id");
+CREATE INDEX ON "reservations" ("email");
+CREATE INDEX ON "reservations" ("last_name");
