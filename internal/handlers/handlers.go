17152253d91c686c2512775f0721c0e148d4481d
chingsley <eneja.kc@gmail.com>
2021-08-30 18:57:52 +0100

    - Connection to the db and adding sql connection to the repo

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -6,10 +6,13 @@ import (
 	"net/http"
 
 	"github.com/chingsley/bookings/internal/config"
+	"github.com/chingsley/bookings/internal/driver"
 	"github.com/chingsley/bookings/internal/forms"
 	"github.com/chingsley/bookings/internal/helpers"
 	"github.com/chingsley/bookings/internal/models"
 	"github.com/chingsley/bookings/internal/render"
+	"github.com/chingsley/bookings/internal/repository"
+	"github.com/chingsley/bookings/internal/repository/dbrepo"
 )
 
 // Repo the repository used by the handlers
@@ -18,12 +21,14 @@ var Repo *Repository
 // Repository is the repository type
 type Repository struct {
 	App *config.AppConfig
+	DB  repository.DatabaseRepo
 }
 
 // NewRepo creates a new repository
-func NewRepo(a *config.AppConfig) *Repository {
+func NewRepo(a *config.AppConfig, db *driver.DB) *Repository {
 	return &Repository{
 		App: a,
+		DB:  dbrepo.NewPostgresRepo(db.SQL, a),
 	}
 }
 
